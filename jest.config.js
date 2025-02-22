8b651bff320bc357a6a73550950f899e2bfc2e7f
Vitali Starastsenka <vstarastsenka@ibagroup.eu>
2025-02-22 22:38:21 +0200

    Add unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test','<rootDir>/product_service'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
