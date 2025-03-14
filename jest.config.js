ef8bfc5ae754de160f2db759b9037fbe9172b487
Vitali Starastsenka <vstarastsenka@ibagroup.eu>
2025-03-14 17:44:55 +0200

    Fix slow jest execution

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/import_service'],
+  transform: {
+    '^.+.tsx?$': ['ts-jest', { isolatedModules: true }]
+  },
+  roots: ['<rootDir>/import_service', '<rootDir>/product_service'],
   testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/cdk.out/'],
 };
